af9e50e397f9967e525440193ea8f6fa6a738fdc
hwang91 <wangheng0394@126.com>
2014-09-03 17:32:38 +0800

    A totally new version

--- radixSort.c
+++ radixSort.c
@@ -1,60 +1,58 @@
-// radixSort.c
+//radixSort.c
 // Wang Heng
 // Sept 3, 2014
-// Sort a single int array
-// No arguments
-
 
 #include <stdio.h>
 
-void COUNTINGSORT(char *A, int *R, int array_size, int k);
+char * Rotate(char *s, int i);
+
+int main(){
+
+  char s[5][5] = 
+      { 
+        "31025",
+        "40123",
+        "42150",
+        "32302",
+        "12433"
+      };
+
+  int rank_before[5], rank_after[5];
+  int i;
+  for(i = 0; i < 5; i++){
+    rank_after[i] = i;
+  }
+
+  for(i = 4; i >= 0; i--){
+
+    int C[6], k, value, pos;
+    // set rank_before to rank_after
+    for(k = 0; k < 5; k++)
+      rank_before[k] = rank_after[k];
+
+    for(k =0; k < 6; k++)
+      C[k] = 0;
+
+    for(k = 0; k < 5; k++)
+      C[s[k][i] - '0']++;
+
+      for(k = 1; k < 6; k++)
+            C[k] = C[k] + C[k-1];
         
-int main()
-{
-        
-   char A[8] = "21302303";
-   int  R[8];
-   int i;
-   // A[8] = {2, 5, 3, 0, 2, 3, 0, 3}, R[8], i;
-   for (i=0; i<= 7; i++){
-      printf("%c", A[i]);
-   }
-   printf("\n");
-
-   COUNTINGSORT(A, R, 8, 5);
-   for (i=0; i<= 7; i++)
-   {
-      printf("%d", R[i]);
-   }
-   printf("\n");
-   
-   return 0;
-}
-
-
-void COUNTINGSORT(char *A, int *R, int array_size, int k)
-{
-        int C[k+1], i, value, pos;
-        for(i=0; i<=k; i++)
-        {
-            C[i] = 0;
-        }
-        for(i=0; i< array_size; i++)
-        {
-            C[A[i] - '0'] ++;
-        }
-        for(i=1; i<=k; i++)
-        {
-            C[i] = C[i] + C[i-1];
+        for(k = 4; k>= 0; k--){
+          value = s[rank_before[k]][i] - '0';
+          pos = C[value];
+          rank_after[pos - 1] = rank_before[k];
+          C[value]--;
         }
 
-        for(i=array_size-1; i>=0; i--)
-        {
-            value = A[i] - '0';
-            pos = C[value];
-            //  B[pos-1] = value;
-            R[pos-1] = i;
-            C[value]--;
+        for(k = 0; k < 5; k++)
+      printf("%d\t", rank_after[k]);
+    printf("\n");
 
-        }
+
+  }
+
+
+  return 0;
 }
--- test.c
+++ test.c
@@ -1,13 +1,54 @@
 #include <stdio.h>
 
+char * Rotate(char *s, int i);
+
 int main(){
-	char s[8] = "21300222";
-	int count[4] = {0};
+
+	char s[5][5] = 
+			{	
+				"31025",
+				"40123",
+				"42150",
+				"32302",
+				"12433"
+			};
+
+	int rank_before[5], rank_after[5];
 	int i;
+	for(i = 0; i < 5; i++){
+		rank_after[i] = i;
+	}
 
-	for (i = 0; i < 8; i++)
-		count[s[i] - '0'] ++;
+	for(i = 4; i >= 0; i--){
 
-	for (i = 0; i < 4; i++)
-		printf("%d\n",count[i]);
+		int C[6], k, value, pos;
+		// set rank_before to rank_after
+		for(k = 0; k < 5; k++)
+			rank_before[k] = rank_after[k];
+
+		for(k =0; k < 6; k++)
+			C[k] = 0;
+
+		for(k = 0; k < 5; k++)
+			C[s[k][i] - '0']++;
+
+	    for(k = 1; k < 6; k++)
+            C[k] = C[k] + C[k-1];
+        
+        for(k = 4; k>= 0; k--){
+        	value = s[rank_before[k]][i] - '0';
+        	pos = C[value];
+        	rank_after[pos - 1] = rank_before[k];
+        	C[value]--;
+        }
+
+       	for(k = 0; k < 5; k++)
+			printf("%d\t", rank_after[k]);
+		printf("\n");
+
+
+	}
+
+
+	return 0;
 }
