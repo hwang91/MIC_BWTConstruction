f44d6b3e2c44a5c714cf48ae7e75051f10795ed4
hwang91 <wangheng0394@126.com>
2014-09-11 16:01:50 +0800

    Working properly for large dataset

--- radixSort.c
+++ radixSort.c
@@ -4,13 +4,17 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 //#define PRINT_PASS
-//#define PRINT_COUNT
+#define PRINT_COUNT
+//#define DEBUG
 
 #define ALPHABETA_SIZE  4
-//#define READ_LEN        8
-//#define READ_NUM        5
+#define MAX_LINE_LENGTH 256
+//#define READ_LEN        4
+//#define READ_NUM        3
+
 
 //char s[READ_NUM][READ_LEN];
 //int  Count[READ_LEN][ALPHABETA_SIZE + 1] = {0}; 
@@ -22,7 +26,18 @@ int main(int argc, char** argv){
     int READ_NUM = atoi(argv[3]);
 
     char* s = (char*)malloc(sizeof(char) * READ_LEN * READ_NUM);
+    if(s == NULL){
+        printf("Fail to alloc memory for sequences. Exiting ... \n");
+        exit(1);
+    }
+    //memset(s, 32, READ_LEN * READ_NUM);
+
     int * Count = (int*)malloc(sizeof(int) * READ_LEN * (ALPHABETA_SIZE + 1));
+    if(Count == NULL){
+        printf("Fail to alloc memory for Count. Exiting ... \n");
+        exit(1);
+    }
+
 
     int i = 0;
     int j = 0;
@@ -33,54 +48,65 @@ int main(int argc, char** argv){
         exit(1);
     }
 
-    char c = fgetc(fp);
-    while(c != EOF){
-        if(c == '\n'){
-            i++ ;
-            j = 0;
-            c = fgetc(fp);
+    char readLine[MAX_LINE_LENGTH];
+    //fgets(readLine, MAX_LINE_LENGTH, fp);
+    while(fgets(readLine, MAX_LINE_LENGTH, fp)){
+        if(readLine[0] == '>') continue;
+        for(j = 0; j < READ_LEN; j++){
+            char c = readLine[j];
+            char tmp = ConvertBase2Int(c);
+            s[i * READ_LEN + j] = tmp;
+            Count[j * (ALPHABETA_SIZE + 1) + (tmp - '0')]++;
         }
-        s[i * READ_LEN + j] = ConvertBase2Int(c);
-        Count[j * READ_LEN + ConvertBase2Int(c) - '0']++;
-        j++ ;
-        c = fgetc(fp);
+        i++;
+        //fgets(readLine, MAX_LINE_LENGTH, fp);
     }
 
-
-    fclose(fp);
-
+/*
+    //Print sequence code
     for(i = 0; i < READ_NUM; i++){
         for(j = 0; j < READ_LEN; j++)
             putchar(s[i * READ_LEN + j]);
         putchar('\n');
     }
-
+*/
     for(i = READ_LEN - 2; i >= 0; i--){
         for(j = 0; j < ALPHABETA_SIZE + 1; j++)
-            Count[i * READ_LEN + j] += Count[(i + 1) * READ_LEN + j];
+            Count[i * (ALPHABETA_SIZE + 1) + j] += Count[(i + 1) * (ALPHABETA_SIZE + 1) + j];
     }
 
     // Calculate # of 0 
     for(j = 0; j < READ_LEN; j++)
-        Count[j * READ_LEN] = READ_LEN * READ_NUM  - Count[j * READ_LEN + 1] - Count[j * READ_LEN + 2] - Count[j * READ_LEN + 3] - Count[j * READ_LEN + 4];
-    
+        Count[j * (ALPHABETA_SIZE + 1)] = READ_LEN * READ_NUM  - Count[j * (ALPHABETA_SIZE + 1) + 1] \
+         - Count[j * (ALPHABETA_SIZE + 1) + 2] - Count[j * (ALPHABETA_SIZE + 1) + 3] - Count[j * (ALPHABETA_SIZE + 1) + 4];
+
     //Accumulation
     for(i = 0; i < READ_LEN; i++){
         for(j = 1; j < ALPHABETA_SIZE + 1; j++)
-            Count[i * READ_LEN + j] += Count[i * READ_LEN + j - 1];
+            Count[i * (ALPHABETA_SIZE + 1) + j] += Count[i * (ALPHABETA_SIZE + 1) + j - 1];
     }
 
 #ifdef PRINT_COUNT
-
     for(i = 0; i < READ_LEN; i++){
+        printf("%d\t", i );
         for(j = 0; j < ALPHABETA_SIZE + 1; j++)
-            printf("%d\t", Count[i * READ_LEN + j]);
+            printf("%d\t", Count[i * (ALPHABETA_SIZE + 1) + j]);
         putchar('\n');
     }
+#endif //PRINT_COUNT
 
-#endif
 
-    int rank_before[READ_LEN * READ_NUM], rank_after[READ_LEN * READ_NUM];
+    int * rank_before = (int*)malloc(sizeof(int) * READ_NUM * READ_LEN);
+    if(rank_before == NULL){
+        printf("Fail to alloc memory for rank_before. Exiting ... \n");
+        exit(1);
+    }
+ 
+    int * rank_after  = (int*)malloc(sizeof(int) * READ_NUM * READ_LEN);
+    if(rank_after == NULL){
+        printf("Fail to alloc memory for rank_after. Exiting ... \n");
+        exit(1);
+    }
 
     for(i = 0; i < READ_LEN * READ_NUM; i++){
         rank_after[i] = i;
@@ -90,41 +116,54 @@ int main(int argc, char** argv){
     for(i = READ_LEN - 1; i >= 0; i--){
         //Count sort
         int k, value, pos;
-        // set rank_before to rank_after
-        for(k = 0; k < READ_LEN * READ_NUM; k++)
-            rank_before[k] = rank_after[k];
-        
 
+        // set rank_before to rank_after
+        for(k = 0; k < READ_LEN * READ_NUM ; k++) 
+            rank_before[k] = rank_after[k];
+
+        //Counting Sort
         for(k = READ_NUM * READ_LEN - 1; k >= 0; k--){
-            value = ((rank_before[k] % READ_LEN + i) >= READ_LEN )? 0 : s[rank_before[k] + i] - '0';
-            pos   = Count[i * READ_LEN + value];
-            rank_after[pos - 1] = rank_before[k];
-            Count[i * READ_LEN + value]--;
+            #ifdef DEBUG
+                printf("%s %d\n", "Debug", k);
+            #endif
+            value = ((rank_before[k] % READ_LEN + i) >= READ_LEN )? 0 : s[rank_before[k] + i] - '0'; 
+            if(value > 4 || value < 0){
+                printf("Error. %d  %d  %d\n", value, i, k);
+                exit(0);
+            }
+            pos   = Count[i * (ALPHABETA_SIZE + 1) + value]; 
+            rank_after[pos - 1] = rank_before[k]; 
+            Count[i * (ALPHABETA_SIZE + 1) + value]--; 
         }
         
     #ifdef PRINT_PASS
-
         for(k = 0; k < READ_LEN * READ_NUM; k++)
             printf("%d\t", rank_after[k]);
-
         printf("\n");
-
     #endif
 
     }
 
+    
+    //Print BWT
     for(i = 0; i < READ_LEN * READ_NUM; i++){
         if(rank_after[i] % READ_LEN == 0) {
             putchar('0');
             continue;
         }
         printf("%c", s[rank_after[i] - 1]);
+        if(!(i % READ_LEN)) putchar('\n');
     }
     printf("\n");
+    
+
+    //free(rank_before); printf("1\n");
+    //free(rank_after);printf("1\n");
+    free(s);
+    free(Count);
 
     return 0;
 }
-
 char ConvertBase2Int(char c){
     char tmp;
     switch(c){
@@ -136,7 +175,7 @@ char ConvertBase2Int(char c){
         case 'g': tmp = '3'; break;
         case 'T': tmp = '4'; break;
         case 't': tmp = '4'; break;
-        default : tmp = '0'; 
+        default : tmp = '5'; 
     }
     return tmp;
 }
